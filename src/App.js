af566967fbff69cf8fb957db0c82e573bb3aec77
shahzadsalamat <shahzad.salamat@yahoo.com>
2020-08-27 22:59:31 +0300

    adding create files

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React, {Component} from 'react';
 import CreateUserForm from './components/createUserForm.js';
 import UsersList from './components/usersList.js';
 import './css/index.css';
+import firebaseDB from './js/firebase.js';
 
 
 class App extends Component {
+
+    addNewUser = (newUserData) => {
+   firebaseDB.database().ref().child('user').push(newUserData);
+}
 
   render() {
 
       return (
           <div className='wrapper'>
-              <CreateUserForm />
+              <CreateUserForm addNewUser={this.addNewUser} />
               <UsersList />
           </div>
       );
--- src/components/createUserForm.js
+++ src/components/createUserForm.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
 
 class CreateUserForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: {
+            }
+        }
+    }
+
+    handleUserFormChange = (e) => {
+        const { name, value } = e.target;
+        this.setState({
+            user: {
+                ...this.state.user,
+                [name]: value,
+            }
+        });
+    }
+
+    handleCreateUserSubmit = (newUserData) => {
+     //   console.log('newUserData' + JSON.stringify(newUserData));
+        this.props.addNewUser(newUserData);
+    }
 
     render() {
 
@@ -10,19 +32,24 @@ class CreateUserForm extends Component {
                     <input
                         className='form-input'
                         type='text'
-                        value=''
-                        placeholder='enter your firstName'
+                        name='username'
+                        placeholder='enter your Name'
+                        value={this.state.user.name}
+                        onChange={this.handleUserFormChange}
                     ></input>
                     <input
                         className='form-input'
                         type='email'
-                        value=''
+                        name='email'
+                        value={this.state.user.email}
                         placeholder='enter your email'
+                        onChange={this.handleUserFormChange}
                     ></input>
                     <input
                         className='form-button bg-green'
                         type='button'
                         value='Add New User'
+                        onClick={this.handleCreateUserSubmit.bind(this, this.state.user)}
                     ></input>
 
                 </form>
