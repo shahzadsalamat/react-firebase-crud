8ba58552e9bd74f37df99e0e51944dda5f2215c2
shahzadsalamat <shahzad.salamat@yahoo.com>
2020-09-08 15:16:23 +0300

    Updates

---
+++ precache-manifest.ef6fbe7f3bf2df1cc6823c6f43b4839e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eafc41e8b3261a0d9894aba990008c86",
+    "url": "/react-firebase-crud/index.html"
+  },
+  {
+    "revision": "76af074030a1fd126759",
+    "url": "/react-firebase-crud/static/css/main.977a3678.chunk.css"
+  },
+  {
+    "revision": "ad43a86a7e9b476fde4c",
+    "url": "/react-firebase-crud/static/js/2.5b424a52.chunk.js"
+  },
+  {
+    "revision": "76af074030a1fd126759",
+    "url": "/react-firebase-crud/static/js/main.c4f04be5.chunk.js"
+  },
+  {
+    "revision": "4d1ec22a44639c268a1c",
+    "url": "/react-firebase-crud/static/js/runtime-main.a93bcabd.js"
+  }
+]);
